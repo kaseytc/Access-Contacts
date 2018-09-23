9157730eeb7b402b9b9fc127e1de1ded52985e3b
kaseytc <16894195+kaseytc@users.noreply.github.com>
2018-09-24 00:09:19 +0800

    initial

---
+++ AccessContacts/ContactStruct.swift
@@ -0,0 +1,15 @@
+//
+//  ContactStruct.swift
+//  AccessContacts
+//
+//  Created by Ting-Chun Yeh on 6/17/18.
+//  Copyright © 2018 Ting-Chun Yeh. All rights reserved.
+//
+
+import Foundation
+
+struct ContactStruct {
+    let givenName: String
+    let familyName: String
+    let number: String?
+}
--- AccessContacts/ViewController.swift
+++ AccessContacts/ViewController.swift
@@ -7,19 +7,60 @@
 //
 
 import UIKit
+import Contacts
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var contactStore = CNContactStore()
+    var contacts = [ContactStruct]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        contactStore.requestAccess(for: .contacts) { (success, error) in
+            if success {
+                print("Autherization Successful")
+            }
+        }
+        fetchContacts()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+        let contactToDisplay = contacts[indexPath.row]
+        cell.textLabel?.text = contactToDisplay.givenName + " " + contactToDisplay.familyName
+        cell.detailTextLabel?.text = contactToDisplay.number
+        return cell
+    }
+    
+    func fetchContacts() {
+        let key = [CNContactGivenNameKey, CNContactFamilyNameKey, CNContactPhoneNumbersKey] as [CNKeyDescriptor]
+        let request = CNContactFetchRequest(keysToFetch: key)
+        try! contactStore.enumerateContacts(with: request) { (contact, stoppingPointer) in
+            let name = contact.givenName
+            let familyName = contact.familyName
+            let number = contact.phoneNumbers.first?.value.stringValue
+            let contactToAppend = ContactStruct(givenName: name, familyName: familyName, number: number)
+            
+            self.contacts.append(contactToAppend)
+        }
+        tableView.reloadData()
+    }
 
 }
 
